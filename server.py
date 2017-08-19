9f2efabb69050e2a5a668cf2d605a4e9e05fc930
John Hammond <johnhammond010@gmail.com>
2017-08-19 06:27:51 -0400

    Fixing stupid string mistakes I missed...

--- server.py
+++ server.py
@@ -130,7 +130,7 @@ def user_login( email ):
 	launch_user_virtual_machine()
 
 def turn_on_user_virtual_machine():
-	subprocess.Popen( 'gotty -w -p ' + str(8080 + flask_login.current_user.id) + ' vagrant up --provision'.split(), cwd = flask_login.current_user.vm_path )
+	subprocess.Popen( str('gotty -w -p ' + str(8080 + flask_login.current_user.id) + ' vagrant up --provision').split(), cwd = flask_login.current_user.vm_path )
 
 def launch_user_virtual_machine():
 	subprocess.Popen( str('gotty -w -p ' + str(8080 + flask_login.current_user.id) + ' vagrant ssh').split(), 
