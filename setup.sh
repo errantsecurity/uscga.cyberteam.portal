549521cbf06bd6bad8348f1eb6eb88b2aeee826a
John Hammond <johnhammond010@gmail.com>
2017-08-19 05:20:51 -0400

    brew/gotty

--- setup.sh
+++ setup.sh
@@ -32,10 +32,10 @@ function install_dependencies(){
 
 	# Brew will only install things if it is owned by root, so...
 	# Kinda silly, but we've gotta do it.
-	sudo chown -R `logname` /usr/local
+	
 
 	# Have brew install gotty
-	su `logname` -c brew install yudai/gotty/gotty
+	su `logname` -c "brew install yudai/gotty/gotty"
 	export PATH=$PATH:$HOME/.linuxbrew/bin/
 	echo "export PATH=$PATH:$HOME/.linuxbrew/bin/" >> ~/.bashrc
 	
