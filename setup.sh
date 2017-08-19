3dc233f72e5412f808935d3178d9aa7f73ab57cc
John Hammond <johnhammond010@gmail.com>
2017-08-19 05:33:01 -0400

    Changing up nginx config file because it didn't like the current setup...

--- setup.sh
+++ setup.sh
@@ -57,9 +57,6 @@ function configure_nginx(){
 server {
 		location / {
 				proxy_pass http://127.0.0.1:8000;
-
-				proxy_set_header Host $header;
-				proxy_set_header X-Real-IP $remote_addr;
 		}
 }
 EOF
